9e423e3c4bca4c2874c32fa4f589875b0044ba9e
DESKTOP-0GO26SD\lucca <luccasmartins.nave@gmail.com>
2017-03-22 04:37:08 -0300

    Adicionado funcao de temporizador no gameplay.

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+	private string currentScene = "Main";
+	public GameObject PlayButton, ShopButton, AchievementsButton, ChallengerButton;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+	private void ResetAllButtons() {
+		PlayButton.GetComponent<tk2dTextMesh>().color = Color.white;
+		ShopButton.GetComponent<tk2dTextMesh>().color = Color.white;
+		AchievementsButton.GetComponent<tk2dTextMesh>().color = Color.white;
+		ChallengerButton.GetComponent<tk2dTextMesh>().color = Color.white;
+	}
+
+	public void SwitchScene(string s) {
+
+		ResetAllButtons();
+
+		switch (s) {
+			case "Play":
+				PlayButton.GetComponent<tk2dTextMesh>().color = Color.red;
+				break;
+
+			case "Shop":
+				ShopButton.GetComponent<tk2dTextMesh>().color = Color.red;
+				break;
+
+			case "Achievements":
+				AchievementsButton.GetComponent<tk2dTextMesh>().color = Color.red;
+				break;
+
+			case "Challenger":
+				ChallengerButton.GetComponent<tk2dTextMesh>().color = Color.red;
+				break;
+		}
+	}
+}
---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour {
+
+	public tk2dClippedSprite UpperBar, RightBar, BottomBar, LeftBar, currentBar;
+	public float stageTime;
+
+	private float currentStageTime, temp;
+	private bool GameOver = false;
+
+	void Start () {
+		currentStageTime = stageTime;
+	}
+	
+	void FixedUpdate() {
+		currentStageTime -= Time.deltaTime;
+		temp += Time.deltaTime;
+
+		if (currentStageTime > stageTime * 0.75f) {
+			currentBar = UpperBar;
+			currentBar.ClipRect = new Rect((1 / (stageTime * 0.25f)) * temp , 0, 1, 1);
+		} else if (currentStageTime > stageTime * 0.5f) {
+			UpperBar.ClipRect = new Rect(1, 0, 1, 1);
+			currentBar = RightBar;
+			currentBar.ClipRect = new Rect((1 / (stageTime * 0.25f)) * (temp-stageTime*0.25f), 0, 1, 1);
+		} else if(currentStageTime > stageTime * 0.25f) {
+			RightBar.ClipRect = new Rect(1, 0, 1, 1);
+			currentBar = BottomBar;
+			currentBar.ClipRect = new Rect((1 / (stageTime * 0.25f)) * (temp-stageTime*0.5f), 0, 1, 1);
+		} else if(currentStageTime > 0f) {
+			BottomBar.ClipRect = new Rect(1, 0, 1, 1);
+			currentBar = LeftBar;
+			currentBar.ClipRect = new Rect((1 / (stageTime * 0.25f)) * (temp-stageTime*0.75f), 0, 1, 1);
+		} else {
+			LeftBar.ClipRect = new Rect(1, 0, 1, 1);
+
+			if (!GameOver) {
+				GameOver = true;
+				iTween.ScaleTo(GameObject.Find("WinnerBanner"), iTween.Hash("x", 1.5, "y", 1.5, "easetype", iTween.EaseType.easeOutElastic, "time", 0.5));
+			}
+		}
+	}
+}
